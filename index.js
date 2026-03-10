b6b59cafce8b381c212f54695364a649f548d077
Gustavo Reis <gustavoferreis.1999@icloud.com>
2026-03-09 22:22:08 -0300

    adiciona documentacao swagger

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
-
 const orderRoutes = require('./src/routes/orderRoutes');
+const { swaggerUi, specs } = require('./src/docs/swagger');
 
 const app = express();
 
 app.use(express.json());
 
 app.use(orderRoutes);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
 const PORT = 3000;
 
---
+++ src/docs/swagger.js
@@ -0,0 +1,23 @@
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "API de Pedidos",
+      version: "1.0.0",
+      description: "API REST para gerenciamento de pedidos",
+    },
+    servers: [
+      {
+        url: "http://localhost:3000",
+      },
+    ],
+  },
+  apis: ["./src/routes/*.js"],
+};
+
+const specs = swaggerJsdoc(options);
+
+module.exports = { swaggerUi, specs };
